2109f3e1ea218dd0308a73d112c4d53dd187593e
Luiz Franzon <luizfranzon05@hotmail.com>
2023-04-19 13:01:54 -0300

    Added google font API and document title

--- App/src/pages/_document.tsx
+++ App/src/pages/_document.tsx
@@ -4,7 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#4a3a52" />
+        <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
       </Head>
       <body className='bg-purple-aurora'>
         <Main />
